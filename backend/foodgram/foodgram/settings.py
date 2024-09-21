0ed8b2a5dc37f065af94defb140b1c391c237371
Дмитрий Горяйнов <goryaynov.dima@mail.ru>
2024-09-21 15:28:13 +0700

    make migrations

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -67,6 +67,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
