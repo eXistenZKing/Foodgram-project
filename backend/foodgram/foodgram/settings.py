2dad869da94467f1c104d25871d4578bf5a87dea
Дмитрий Горяйнов <goryaynov.dima@mail.ru>
2024-09-24 22:34:34 +0700

    tht

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -126,8 +126,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '200/day',
-        'user': '1000/day'
+        'anon': '1000/day',
+        'user': '7000/day'
     }
 }
 
